ce491a7b26c24240fcf686cd43177f7a2dc6203c
Sergey <thefanthomasfan@gmail.com>
2021-11-19 19:43:41 +0100

    Might as well do day 2

---
+++ 2020/Day 2/README.md
@@ -0,0 +1,6 @@
+# Day 2
+
+Kotlin, I choose you!
+
+The code is not very pretty, my goal was playing around with higher order functions but ended up with clunky code. \
+For both parts, go through the input set and increase counter if the password is valid, which is O(n).
---
+++ 2020/Day 2/day2.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+fun main() {
+    val passwords = mutableListOf<List<String>>()
+    rff("input.txt", passwords)
+    part_one(passwords)
+    part_two(passwords)
+}
+
+
+fun part_one(passwords: MutableList<List<String>>) {
+    var sum = 0
+    passwords.forEach { p ->
+        if (p[3].filter { it == p[2][0] }.count() in p[0].toInt()..p[1].toInt()) sum++
+    }.also{ println(sum) }
+}
+
+
+fun part_two(passwords: MutableList<List<String>>) {
+    var sum = 0
+    passwords.forEach { p ->
+        val first = p[3][p[0].toInt()-1]
+        val last = p[3][p[1].toInt()-1]
+        val n = p[2][0]
+        
+        if ((first == n) xor (last == n)) sum++
+    }.also{ println(sum) }
+}
+
+
+// Helper function for reading from file
+fun rff(fileName: String, lof: MutableList<List<String>>) =
+    File(fileName).forEachLine { lof.add(it.split("-", ": ", " ")) }
