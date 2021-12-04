d9cd2d34dea45815118af4bdc4a730f2ebd40c5f
Sergey <thefanthomasfan@gmail.com>
2021-12-04 15:41:16 +0100

    Day 4 with OO Python

---
+++ 2021/Day 4/README.md
@@ -0,0 +1,9 @@
+# Day 4
+Todays puzzle reminds me a lot of an oblig I did this semester, where I had to solve an 8-puzzle with the A* algorithm. I used OO to facilitate working on the board, and I figured I would do the same here. Slightly unhappy with the inconsistency between higher order functions and list comprehension, but other than that, I'm at ease with the overall solution :slightly_smiling_face:
+
+## The solution
+Each board is really just an array of strings, and revolves around the checkWinner-function, which checks whether an entire row or column has been marked. For every number in the bingo-list, we mark every board that contains the current number, by replacing it with an 'M'.
+
+For part 1, we print the product of the first winners unmarked tiles and the current number. I assume the time complexity must be O(n * b), since we look at every board for each bingo-number.
+
+For part 2, we print the product of the *last* winners unmarked tiles and the current number. Somehow this was much larger than the first one. The time complexity will be the same, but this is the actual worst case scenario!
---
+++ 2021/Day 4/day4.py
@@ -0,0 +1,69 @@
+class Board:
+    def __init__(self, board):
+        self.board = list(filter(lambda x: x != '', board))
+
+    def checkWinner(self):
+        for i in range(5):
+            if self.getRow(i).count('M') == 5 or \
+               self.getColumn(i).count('M') == 5:
+                return True
+        return False
+
+    def getRow(self, n):
+        return [self.board[i] for i in range(n*5, n*5+5)]
+
+    def getColumn(self, n):
+        return [self.board[i] for i in range(len(self.board)) if i % 5 == n]
+
+    def getSum(self):
+        return sum(map(int, filter(lambda x: x != 'M', self.board)))
+
+
+def readInput(file):
+    with open(file, 'r') as input:
+
+        # The winning numbers
+        numbers = input.readline()[:-1].split(',')
+        input.readline()
+
+        # All boards
+        boards = list()
+        curr = list()
+        for i in input:
+            if i[0] == '\n':
+                boards.append(Board(curr))
+                curr = list()
+                continue
+            
+            for n in i[:-1].split(' '):
+                curr.append(n)
+
+    return numbers, boards
+
+
+def main():
+    numbers, boards = readInput("input.txt")
+    winners = list()
+
+    for n in numbers:
+        for b in boards:
+            if n in b.board:
+                b.board = ['M' if x == n else x for x in b.board]
+
+            if not winners:
+                # Part 1
+                if b.checkWinner():
+                    print(b.getSum() * int(n))
+                    winners.append(b)
+            else:
+                # Part 2
+                if b.checkWinner() and b not in winners:
+                    winners.append(b)
+
+                    if len(boards) == len(winners):
+                        print(b.getSum() * int(n))
+                        return
+
+
+if __name__ == "__main__":
+    main()
