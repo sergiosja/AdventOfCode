4ebefe960b7fe652c64281c8bcef815b289db9cb
sergiosja <sergey.jakobsen@gmail.com>
2024-12-02 16:34:52 +0100

    day 2

---
+++ 2024/Day 2/Program.fs
@@ -0,0 +1,25 @@
+open System.IO
+
+let asc pairs =
+    if List.forall (fun (x, y) ->
+        List.contains (x - y) [-1; -2; -3]) pairs then 1 else 0
+
+let desc pairs =
+    if List.forall (fun (x, y) ->
+        List.contains (x - y) [1; 2; 3]) pairs then 1 else 0
+
+let input =
+    File.ReadAllLines("input.txt")
+    |> Array.map (fun line -> line.Split ' ' |> Array.map int |> Array.toList)
+    |> Array.toList
+
+let part1 =
+    input
+    |> List.map (fun lst -> List.pairwise lst)
+    |> List.map (fun pairs ->
+        match pairs.[0] with
+        | x, y when x < y -> asc pairs
+        | x, y when x > y -> desc pairs
+        | _ -> 0)
+    |> List.sum
+    |> printfn "%d"
---
+++ 2024/Day 2/README.md
@@ -0,0 +1,9 @@
+# Day 2
+
+Day 2 and I'm already defeated
+
+## Solution
+
+The sliding window approach works perfectly for the first part: based on the first pair we check whether or not the remaining pairs are within the required limits.
+
+The second part turnet my world upside down and I still haven't recovered.
